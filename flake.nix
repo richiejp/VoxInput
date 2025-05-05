c0ee1e6d461fdee7694593b3eecf55f41a93da41
Richard Palethorpe <io@richiejp.com>
2025-05-05 23:03:05 +0100

    Init with malgo demo

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "UseDesk";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+            buildInputs = with pkgs; [
+              go
+              gopls
+              gotools
+              go-tools
+
+            ];
+          };
+        });
+    };
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/richiejp/UseDesk
+
+go 1.24.2
+
+require github.com/gen2brain/malgo v0.11.23
