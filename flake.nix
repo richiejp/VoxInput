aa133f0b4fcbc6a6e5c4e14bd86edc4c90bd8087
Richard Palethorpe <io@richiejp.com>
2025-06-08 20:00:43 +0100

    chore(nix): Guard install/fixup to keep in sync with Nix package

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in
@@ -33,6 +33,7 @@
         let
           pkgs = nixpkgsFor.${system};
           lib = pkgs.lib;
+          stdenv = pkgs.stdenv;
         in
         {
           default = pkgs.buildGoModule {
@@ -55,14 +56,16 @@
             ];
 
             postInstall = ''
-              mv $out/bin/VoxInput $out/bin/voxinput
+              mv $out/bin/VoxInput $out/bin/voxinput_tmp ; mv $out/bin/voxinput_tmp $out/bin/voxinput
+            ''
+            + lib.optionalString stdenv.hostPlatform.isLinux ''
               wrapProgram $out/bin/voxinput \
                 --prefix PATH : ${lib.makeBinPath [ pkgs.dotool ]}
               mkdir -p $out/lib/udev/rules.d
               echo 'KERNEL=="uinput", GROUP="input", MODE="0620", OPTIONS+="static_node=uinput"' > $out/lib/udev/rules.d/99-voxinput.rules
             '';
 
-            postFixup = ''
+            postFixup = lib.optionalString stdenv.hostPlatform.isElf ''
               patchelf $out/bin/.voxinput-wrapped \
                 --add-rpath ${lib.makeLibraryPath [ pkgs.libpulseaudio ]}
             '';
